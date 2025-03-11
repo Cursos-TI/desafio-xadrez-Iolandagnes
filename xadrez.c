dcec051947b5389ab3c686caeafd4af98411ef4a
Iolanda Agnes <116354944+Iolandagnes@users.noreply.github.com>
2025-03-11 19:07:19 +0000

    Nível mestre - recursividade e loops

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+//Implementação dos procedimentos do nível mestre
+
+//Implementação da Torre - Cinco casas para a direita
+void moverTorre (int casas) {
+    if (casas > 0) {
+        moverTorre(casas - 1);
+        printf("Movimento %d para direita\n", casas);
+    }
+}
+
+//Implementação da Rainha - oito casas para a esquerda
+void moverRainha (int casas) {
+    if (casas > 0) {
+        moverRainha(casas - 1);
+        printf("Movimento %d para esquerda\n", casas);
+    }
+}
+
+// Implementação de Movimentação do Bispo - Cinco casas na diagonal para cima e à direita
+void moverBispo (int casas) {
+    if (casas > 0) {
+        moverBispo(casas - 1);
+        printf("Movimento %d para cima e à direita\n", casas);
+    }
+}
+
+
 int main() {
-    // Nível Novato - Movimentação das Peças
     int i, j = 1, k = 1, l = 1, m;
+    int casasTorre = 5, casasRainha = 8, casasBispo = 5;
+
+    // Nível Novato - Movimentação das Peças
 
     printf("\nDesafio de Xadrez - MateCheck\n");
 
@@ -35,15 +64,29 @@ int main() {
         for(m = 1; m <= 2; m++) {
             printf("Movimento %d para baixo\n", m);
         }
-        printf("Movimento 1 perpendicular para a esquerda");
+        printf("Movimento 1 perpendicular para a esquerda\n");
     }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    printf("\nNível Mestre - movimentações\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("\nMovimentação da Torre\n");
+    moverTorre(casasTorre);
+
+    printf("\nMovimentação da Rainha\n");
+    moverRainha(casasRainha);
+
+    printf("\nMovimentação do cavalo\n");
+    for (int aux = 1; aux <= 1; aux++)
+    {
+        for(m = 1; m <= 2; m++) {
+            printf("Movimento %d para cima\n", m);
+        }
+        printf("Movimento %d perpendicular para a direita\n", aux);
+    }
+
+    printf("\nMovimentação do Bispo\n");
+    moverBispo(casasBispo);
 
     return 0;
 }
